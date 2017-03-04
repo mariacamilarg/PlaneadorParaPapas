23008ea79f963080673cde55b32cf80885a8c916
camendoza94 <ca.mendoza968@uniandes.edu.co>
2017-03-04 16:32:07 -0500

    server.js ready for Heroku depployment

--- ppp/backend/server.js
+++ ppp/backend/server.js
@@ -1,22 +1,18 @@
 // Modules
 const bodyParser = require('body-parser');
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+var mongoose = require('mongoose');
 
 const routes = require('./routes/index');
 
 // Vars
-let port = 5030;
 let app = express();
 
-app.use(morgan('dev'));
-app.use(cors());
 app.use(bodyParser.json());
 
 routes(app);
 
 
-app.listen(port, () => {
-  console.log('Started in port ' + port);
+app.listen(process.env.PORT || 8080, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
