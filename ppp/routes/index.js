8a3c786037db80c32eb2f0269c4d27b0e066823f
camendoza94 <ca.mendoza968@uniandes.edu.co>
2017-03-04 12:25:45 -0500

    Added .gitignore, changed view to Jade

--- ppp/routes/index.js
+++ ppp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Planeador Para Papas' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
